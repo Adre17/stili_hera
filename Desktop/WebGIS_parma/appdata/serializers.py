1f04e305d89a1b5fb4afb0cd25f435810dc5f90e
Adre17 <55452794+Adre17@users.noreply.github.com>
2023-08-04 15:40:38 +0200

    Delete Desktop/WebGIS_parma directory

--- Desktop/WebGIS_parma/appdata/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import prove_geognostiche
-
-class ProveSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = prove_geognostiche
-        fields = ('id', 'geom')
-        #fields = '__all__' #per portare tutti i campi del modello
